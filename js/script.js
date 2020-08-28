8eb5b343f4a2d674881531c0d70124acb464f191
gabdykalyk <gabdykalyk@gmail.com>
2020-08-29 00:39:35 +0600

    animation added, registered page completed

--- js/script.js
+++ js/script.js
@@ -8,30 +8,57 @@ const svg = new Vivus(
 );
 
 // VALIDATION
-let formEmail = document.querySelector('#formEmail')
-let message = document.querySelector('.error-msg')
-let password = document.querySelector('#formPassword')
-formEmail.addEventListener('input', () => {
-    if (!formEmail.validity.valid) {
-        formEmail.classList.add('error')
-    }
+function formValidation() {
+	let formEmail = document.querySelector('#formEmail')
+	let message = document.querySelector('.error-msg')
+	let password = document.querySelector('#formPassword')
+	formEmail.addEventListener('input', () => {
+		if (!formEmail.validity.valid) {
+			formEmail.classList.add('error')
+		}
 
-    if (formEmail.validity.valid) {
-        formEmail.classList.remove('error')
-    }
-})
+		if (formEmail.validity.valid) {
+			formEmail.classList.remove('error')
+		}
+	})
 
-password.addEventListener('input', () => {
-    if (!password.validity.valid) {
-        password.classList.add('error')
-        message.style.opacity = '1'
-    }
+	password.addEventListener('input', () => {
+		if (!password.validity.valid) {
+			password.classList.add('error')
+			message.style.opacity = '1'
+		}
 
-    if (password.validity.valid) {
-        password.classList.remove('error')
-        message.style.opacity = '0'
-    }
-})
+		if (password.validity.valid) {
+			password.classList.remove('error')
+			message.style.opacity = '0'
+		}
+	})
+}
+
+formValidation()
+
+// COMPLETE
+function registered() {
+	let complete = document.querySelector('#completeBtn')
+	let formContainer = document.querySelector('#formContainer')
+	let registered = document.querySelector('#registered')
+	complete.addEventListener('click', (e) => {
+		e.preventDefault()
+		formContainer.style.display = 'none'
+		registered.style.display = 'block'
+	})
+}
+
+registered()
+
+function btnError() {
+	let complete = document.querySelector('#completeBtn')
+	complete.addEventListener('click', (e) => {
+		e.preventDefault()
+		complete.classList.toggle('btn-error')
+	})
+}
+
 ;
 /**
  * vivus - JavaScript library to make drawing animation on SVG
