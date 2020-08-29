cf0760093c992655179796f282298a9ec239fda0
Oral <64807609+gabdykalyk@users.noreply.github.com>
2020-08-30 00:02:01 +0600

    Update script.js

--- dist/js/script.js
+++ dist/js/script.js
@@ -74,7 +74,7 @@ function btnError() {
 
 //JSON OK
 function okJson() {
-	var requestURL = '../json/server-ok.json';
+	var requestURL = '../test/json/server-ok.json';
 	var request = new XMLHttpRequest();
 	request.open('GET', requestURL);
 	request.responseType = 'json';
